3d585052913e9b1216efd8940465efb4ca611348
LeonardoFreitas1 <leo_paulino2011@hotmail.com>
2019-12-20 03:54:50 -0300

    Calculadora

---
+++ Calculadora/Calculator.dpr
@@ -0,0 +1,14 @@
+program Calculator;
+
+uses
+  Vcl.Forms,
+  Calculadora in 'Calculadora.pas' {frmCalculator};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmCalculator, frmCalculator);
+  Application.Run;
+end.
